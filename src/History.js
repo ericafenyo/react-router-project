503875c3ff2cfa8375bfc49098dc3d0fcfaa3702
ericafenyo <ericafenyo@yahoo.com>
2019-03-23 18:44:25 +0100

    update code to  enable navigateions between one component to another.

---
+++ src/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class History extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Notre histoire</h4>
+      </div>
+    )
+  }
+}
+
+export default History
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Bienvenue sur l'accueil</h4>
+      </div>
+    )
+  }
+}
+
+export default Home
